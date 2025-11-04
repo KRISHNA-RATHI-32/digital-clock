6ca0a3c9ab67b0853c7c2085497ff65043c76875
KRISHNA-RATHI-32 <krishna.r1042006@gmail.com>
2025-11-05 00:19:21 +0530

    added the project

---
+++ chaiaurcode.js
@@ -0,0 +1,7 @@
+const clock=document.getElementById('clock')
+// document.querySelector('#clock')
+// console.log(date.toLocaleTimeString());
+setInterval(function(){
+    let date=new Date();
+
+    clock.innerHTML=date.toLocaleTimeString();},1000);
